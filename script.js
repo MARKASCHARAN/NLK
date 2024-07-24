eb1aa80ceb09470e73dff6eb83c443b1a0c2efaa
MARKASCHARAN <markascharan@gmail.com>
2024-07-24 17:29:10 +0530

    ADD

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const videoPlayer = document.getElementById('videoPlayer');
     const videos = [
-        'vids/vid3.mp4',
+        'vids/vid4.mp4',
         'vids/vid5.mp4',
         'vids/vid1.mp4',
         'vids/vid6.mp4',
-        'vids/vid8.mp4'
+        'vids/vid8.mp4',
     ];
     let currentVideoIndex = 0;
     let videoTimeout;
 
     videoPlayer.addEventListener('play', function() {
-        
         clearTimeout(videoTimeout); // Clear any existing timeout to avoid overlapping
         videoTimeout = setTimeout(fadeOutVideo, 5000); // 5000 ms = 5 seconds
     });
@@ -30,12 +29,14 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         videoPlayer.src = videos[currentVideoIndex];
         videoPlayer.classList.remove('fade-out');
-        videoPlayer.play();
+        videoPlayer.addEventListener('canplay', function() {
+            videoPlayer.play();
+        }, { once: true });
     }
 
-    
     videoPlayer.src = videos[currentVideoIndex]; // Start the first video
-    videoPlayer.play();
+    videoPlayer.addEventListener('canplay', function() {
+        videoPlayer.play();
+    }, { once: true });
 });
-
 
